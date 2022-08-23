5e2f3c6d2bf576dfcf72b15f374ffed56c5baaf6
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-08-23 18:31:58 +0900

    Get and print pull requests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Thank you for your contribution");
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+async function run() {
+    const {GITHUB_REPOSITORY} = process.env;
+    const [owner, repo] = GITHUB_REPOSITORY.split("/");
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token)
+    const {data: pulls} = await octokit.rest.pulls.list({
+        owner,
+        repo,
+    });
+
+    console.log(pulls);
+}
+
+run();
