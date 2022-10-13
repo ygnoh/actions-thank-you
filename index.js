04226a1d2f53eb963c17f0e4ea9abcdc99a78bb3
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-10-13 15:25:25 +0900

    Create a comment based on the environment

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const github = require("@actions/github");
 const core = require("@actions/core");
 
 async function run() {
-    const {GITHUB_REPOSITORY} = process.env;
+    const {GITHUB_REPOSITORY, GITHUB_REF} = process.env;
+    const prNum = GITHUB_REF.match(/^refs\/pull\/(.+)\/merge$/)[1];
     const [owner, repo] = GITHUB_REPOSITORY.split("/");
     const token = core.getInput("token");
     const octokit = github.getOctokit(token)
     const {data: pulls} = await octokit.rest.issues.createComment({
         owner,
         repo,
-        issue_number: 10,
+        issue_number: prNum,
         body: "thank you"
     });
 
