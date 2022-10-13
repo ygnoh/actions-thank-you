64230fb722406d84f2023aefc730ff893d0790d4
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-10-13 15:41:37 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 
-async function run() {
-    const {GITHUB_REPOSITORY, GITHUB_REF} = process.env;
-    const prNum = GITHUB_REF.match(/^refs\/pull\/(.+)\/merge$/)[1];
-    const [owner, repo] = GITHUB_REPOSITORY.split("/");
-    const token = core.getInput("token");
-    const octokit = github.getOctokit(token)
+function getOwner() {
+    const {GITHUB_REPOSITORY} = process.env;
 
+    return GITHUB_REPOSITORY.split("/")[0];
+}
+
+function getRepo() {
+    const {GITHUB_REPOSITORY} = process.env;
+
+    return GITHUB_REPOSITORY.split("/")[1];
+}
+
+function getPRNum() {
+    const {GITHUB_REF} = process.env;
+
+    return GITHUB_REF.match(/^refs\/pull\/(.+)\/merge$/)[1];
+}
+
+function getOctokit() {
+    const token = core.getInput("token");
+
+    return github.getOctokit(token);
+}
+
+function createCommentPayload() {
+    return {
+        owner: getOwner(),
+        repo: getRepo(),
+        issue_number: getPRNum(),
+        body: "Thank you for your contribution!"
+    };
+}
+
+function createComment() {
+    return getOctokit().rest.issues.createComment(createCommentPayload());
+}
+
+async function run() {
     try {
-        await octokit.rest.issues.createComment({
-            owner,
-            repo,
-            issue_number: prNum,
-            body: "Thank you for your contribution!"
-        });
+        await createComment();
     } catch (e) {
         core.setFailed(e);
     }
